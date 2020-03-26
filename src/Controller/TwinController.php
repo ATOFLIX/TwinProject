2244efb2b3f532aa6b3aa862830791823be3cdd4
Benoît Fraj <frajbenoit@hotmail.fr>
2020-03-26 20:56:33 +0100

    1er Save -- Projet Jumeau Numérique Interface

---
+++ src/Controller/TwinController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TwinController extends AbstractController
+{
+    /**
+     * @Route("/twin", name="twin")
+     */
+    public function index()
+    {
+        return $this->render('twin/index.html.twig', [
+            'controller_name' => 'TwinController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('twin/home.html.twig');
+    }
+}
