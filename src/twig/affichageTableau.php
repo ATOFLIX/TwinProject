f779d7b65894f5745737c0f76298763903224fd0
Adri82000 <60610479+Adri82000@users.noreply.github.com>
2020-06-22 20:13:45 +0200

    Update affichageTableau.php

--- src/twig/affichageTableau.php
+++ src/twig/affichageTableau.php
@@ -19,7 +19,7 @@ class affichageTableau extends AbstractExtension
     public function chunkTableauFilter($content)
     {
         //$nom1=explode(",", $content);
-        $tableau=array_chunk($content, 6);
+        $tableau=array_chunk($content, 6);  //https://www.php.net/manual/fr/function.array-chunk.php : sépare le tableau $content en plusieurs tableaux ayant chacun 6 valeurs
         return $tableau;
     }
     
